044868e9280886eb4cd5f421592fc742ad8c6bb6
Matt Johnson <matt@satchamo.com>
2016-02-26 16:46:40 -0800

    Use the right versions of elasticsearch and elasticsearch-dsl (and change the author)

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from setuptools import find_packages, setup
 setup(
     name='elasticmodels',
     version='0.1.0',
-    install_requires=['elasticsearch','elasticsearch-dsl'],
+    install_requires=['elasticsearch<2.0.0','elasticsearch-dsl<2.0.0'],
     packages=find_packages(),
     long_description=open('README.md').read(),
-    author='Andrew Stoneman',
+    author='Matt Johnson',
     extras_require={
         'dev': [
             "mock",
